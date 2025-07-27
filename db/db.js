0510a0a07a4adbcdc60a668b8851ea2a27db1dcc
Mukesh-15 <mukeshdoodapaka@gmail.com>
2025-07-28 00:28:49 +0530

    db issue fixed

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://localhost:27017/chat-app';
+const mongoURI = process.env.MONGO_URI;
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI).then(()=>console.log("connected to database")).catch((e)=>console.log(e.message));
